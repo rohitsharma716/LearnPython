eb620353f4501f85b905f1c77859ae959c991f64
ROHIT SHARMA <102727841+rohitsharma716@users.noreply.github.com>
2025-03-27 12:39:55 +0530

    input,for loop, arr

---
+++ day01.py
@@ -0,0 +1,27 @@
+# input1  = int(input("input 1: "))
+# input2  = int(input("input 2: "))
+
+# sum  =  input1 + input2
+# if (sum > 10):
+#   print("sum is greater than 10")
+# else:
+#   print("sum  is less than 10") 
+# print("sum is : ", sum )
+
+
+# count = 0
+# while count < 6:
+#      print(count)
+#      count+=1
+
+
+# for i in range(5):
+#      print("nodejs" ,i)
+
+
+# for i in range(1,11,2):
+#      print(i)
+
+# phones = ["apple","nokia","redmi"]
+# for phone in phones :
+#     print(phone)
